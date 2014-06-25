39a16c04b51b08902bcf9d902e40289905c6f130
Thomas Marshall <thomas@thomasmarshall.com>
2014-06-25 23:23:38 +0100

    Knock back past events

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var today = new Date().setHours(0,0,0,0);
+
+$('[data-date]').each(function() {
+  var el   = $(this),
+      date = new Date(el.data('date')).setHours(0,0,0,0);
+
+  if (date < today) el.addClass('old');
+});
