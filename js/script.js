fc2f49c8c63e6975f8d32efa4e7084488fd3304e
Thomas Marshall <thomas@thomasmarshall.com>
2014-07-03 12:40:52 +0100

    Highlight today in block events

--- js/script.js
+++ js/script.js
@@ -6,4 +6,11 @@ $('[data-date]').each(function() {
 
   if (date < today) el.addClass('old');
   if (date == today) el.addClass('today');
+
+  if (el.data('start') && el.data('end')) {
+    var start = new Date(el.data('start')).setHours(0,0,0,0),
+        end   = new Date(el.data('end')).setHours(0,0,0,0);
+
+    if (start <= today && today <= end) el.addClass('today');
+  }
 });
